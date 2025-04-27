2cb7f7077fab297f7c67564c773620ddc0b758c8
ruiange <bombingpig@outlook.com>
2025-04-27 17:51:32 +0800

    refactor(contact): 重构获取联系信息接口
    - 添加异常捕获逻辑，提高接口稳定性
    -从 hubKV() 获取的 contactInfo 直接作为响应数据返回- 调整状态码为 2000（成功）和 5000（失败）
    - 优化代码结构，提高可读性和可维护性

--- server/api/contact/info.get.ts
+++ server/api/contact/info.get.ts
@@ -1,12 +1,16 @@
 export default eventHandler(async () => {
-    const contactInfo = await hubKV().get('contact-info')
-    console.log(contactInfo)
-    return {
-        code: 200,
-        data: {
-            phone: '18513687900',
-            address: '北京市丰台区丰科路万达广场写字楼C座517-518',
-            email: 'andess@andess.com',
+    try{
+        const contactInfo = await hubKV().get('contact-info')
+        console.log(contactInfo)
+        return {
+            code: 2000,
+            data: contactInfo
+        }
+    }catch (e:any) {
+        return {
+            code: 5000,
+            data: null,
+            message: e.message
         }
     }
 })
