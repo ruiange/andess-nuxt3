9f0290beefe12dadcc67bb9e25b32538e7763c03
ruiange <bombingpig@outlook.com>
2025-04-27 17:46:59 +0800

    chore(server): 添加联系信息日志输出
    - 在获取联系信息后添加了控制台日志输出，便于调试和监控

--- server/api/contact/info.post.ts
+++ server/api/contact/info.post.ts
@@ -1,7 +1,7 @@
 export default eventHandler(async () => {
     try{
         const contactInfo = await hubKV().get('contact-info')
-
+    console.log(contactInfo)
         return {
             code: 2000,
             data: contactInfo
