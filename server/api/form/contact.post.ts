33044c6dd1afeed1dd6e53e78919e857ea5d0de4
ruiange <bombingpig@outlook.com>
2025-04-27 18:35:40 +0800

    feat(server): 添加联系表单功能并更新 .gitignore
    - 实现联系表单提交功能，包括数据验证和数据库插入
    - 更新 .gitignore 文件，排除 migrations目录

--- server/api/form/contact.post.ts
+++ server/api/form/contact.post.ts
@@ -1,8 +1,30 @@
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
+  console.log(await readBody(event));
+
+  const { name, email, subject, message } = await readBody(event);
+
+  if (!email) {
     return {
-        status: 200,
-        body: {
-            message: 'pong'
-        }
-    }
-})
+      code: 4000,
+      message: '邮箱不能为空',
+    };
+  }
+
+  const result = await useDrizzle()
+    .insert(tables.contactForms)
+    .values({
+      name,
+      email,
+      subject,
+      message,
+      createdAt: new Date(),
+    })
+    .returning()
+    .get();
+
+  return {
+    code: 2000,
+    data: result,
+    message: '提交成功',
+  };
+});
